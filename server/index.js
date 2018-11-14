db6df6f3b40972bdb79f78d464e83dfde8728f8a
joecosta117 <joecosta117@gmail.com>
2018-11-14 17:53:40 -0500

    implementing firebase

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
+// const firebase = require('firebase/app')
 module.exports = app
 
 // This is a global Mocha hook, used for resource cleanup.
