be21d76abf4c2df3ec1f43c3bbfa876595be8814
Sean Ryan <sjr765@gmail.com>
2018-11-27 00:27:21 -0500

    added song preview, most of the pointer functionality

--- client/components/main/SongSelect.js
+++ client/components/main/SongSelect.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
 import anime from 'animejs'
 import Wad from 'web-audio-daw'
+import {Modal} from '../menu/Modal'
+import {selectSong} from '../../store'
 
 class SongSelection extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      song: new Wad({source: '/songs/dancingqueen.m4a'})
+      song: new Wad({source: '/songs/dancingqueen-preview.m4a'}),
+      show: false
     }
     this.enterPoly = this.enterPoly.bind(this)
     this.exitPoly = this.exitPoly.bind(this)
@@ -34,6 +39,20 @@ class SongSelection extends Component {
       duration: 200
     })
   }
+
+  showModal = () => {
+    this.setState({show: true})
+  }
+
+  hideModal = () => {
+    this.setState({show: false})
+  }
+
+  confirmSound() {
+    const confirmSound = new Audio('/assets/game-start.ogg')
+    confirmSound.play()
+  }
+
   render() {
     return (
       <div id="svgAttributes">
@@ -44,12 +63,17 @@ class SongSelection extends Component {
           viewBox="0 0 128 128"
           onMouseEnter={this.enterPoly}
           onMouseLeave={this.exitPoly}
+          onClick={() => {
+            this.showModal()
+            this.props.selectSong('dancing-queen')
+            this.confirmSound()
+          }}
         >
-
           <polygon
             points="64 68.86333111206183 8.574 99.98495073368855 63.23955416473961 67.54576054450193 64 3.9849507336885557 64.7604458352604 67.54576054450193 119.426 99.98495073368855 "
             fill="currentColor"
           />
+
           <defs>
             <clipPath id="clip">
               <polygon points="64 68.86333111206183 8.574 99.98495073368855 63.23955416473961 67.54576054450193 64 3.9849507336885557 64.7604458352604 67.54576054450193 119.426 99.98495073368855 " />
@@ -65,9 +89,21 @@ class SongSelection extends Component {
           />
           <span>ES 335 Cherry</span>
         </svg>
+        {this.state.show && (
+          <Modal
+            show={this.state.show}
+            showModal={this.showModal}
+            hideModal={this.hideModal}
+          />
+        )}
       </div>
     )
   }
 }
 
-export default SongSelection
+const mapDispatchToProps = dispatch => {
+  return {
+    selectSong: song => dispatch(selectSong(song))
+  }
+}
+export default connect(null, mapDispatchToProps)(SongSelection)
