c5ec1df38fd2ae1c560690a3131d73927f7252c4
joecosta117 <joecosta117@gmail.com>
2018-11-28 12:42:29 -0500

    finished the end menu

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/users', require('./users'))
+// router.use('/users', require('./users'))
 router.use('/leaderboard', require('./leaderboard'))
 router.use('/songs', require('./songs'))
 
